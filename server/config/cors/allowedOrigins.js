4d5571d63a5503b6e285e06ebf678aeb3062ea9e
AubreyAmoah <2gee419@gmail.com>
2024-10-23 06:58:15 +0000

    cors changes

--- server/config/cors/allowedOrigins.js
+++ server/config/cors/allowedOrigins.js
@@ -7,8 +7,11 @@ const allowedOrigins = [
   "http://127.0.0.1:3500",
   "http://localhost:3000",
   "http://127.0.0.1:3000",
+  "http://shopclient2024.vercel.app/",
   "https://shopclient2024.vercel.app/",
+  "http://shopadmin2024.netlify.app/",
   "https://shopadmin2024.netlify.app/",
+  "http://shop-y73n.onrender.com",
   "https://shop-y73n.onrender.com",
 ];
 
